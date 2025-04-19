9b12e8b1b0886a9db75f683ed2ed0d53a630fb86
Shiv-am-04 <shivamghuge01@gmail.com>
2025-04-19 17:45:21 +0530

    new version of data added to dvc

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-
 data = pd.read_csv(r'DVC\data\sample.csv')
 
+data.reset_index(drop=True,inplace=True)
 
 # Adding new rows to data for version v2
 new_row = {
@@ -13,4 +13,13 @@ new_row = {
     'Salary' : 14000
 }
 
-data.loc[len(data)] = new_row
+data.loc[data.shape[0]] = new_row
+
+print(data)
+
+
+
+# save the changes to data
+data.to_csv(r'DVC\data\sample.csv',index=False)
+
+print(f"changes made to the data")
