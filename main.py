62059169c4d38653ad551d462431107fd08232d9
Shiv-am-04 <shivamghuge01@gmail.com>
2025-04-19 16:35:19 +0530

    created main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv(r'DVC\data\sample.csv')
+
+
+# Adding new rows to data for version v2
+new_row = {
+    'Name' : 'Rina',
+    'Place' : 'Pune',
+    'Age' : 25,
+    'Salary' : 14000
+}
+
+data.loc[len(data)] = new_row
